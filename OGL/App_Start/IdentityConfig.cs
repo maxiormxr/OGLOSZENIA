1f6369c19f993affd69ca6b167657c69ab57a4c6
maxiormxr <mateuszmachcinski@gmail.com>
2019-05-01 21:41:05 +0200

    Dodaj pliki projektów.

---
+++ OGL/App_Start/IdentityConfig.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using OGL.Models;
+
+namespace OGL
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Dołącz tutaj usługę poczty e-mail, aby wysłać wiadomość e-mail.
+            return Task.FromResult(0);
+        }
+    }
+
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Dołącz tutaj usługę wiadomości SMS, aby wysłać wiadomość SMS.
+            return Task.FromResult(0);
+        }
+    }
+
+    // Skonfiguruj menedżera użytkowników aplikacji używanego w tej aplikacji. Interfejs UserManager jest zdefiniowany w produkcie ASP.NET Identity i jest używany przez aplikację.
+    public class UzytkownikManager : UserManager<Uzytkownik>
+    {
+        public UzytkownikManager(IUserStore<Uzytkownik> store)
+            : base(store)
+        {
+        }
+
+        public static UzytkownikManager Create(IdentityFactoryOptions<UzytkownikManager> options, IOwinContext context) 
+        {
+            var manager = new UzytkownikManager(new UserStore<Uzytkownik>(context.Get<ApplicationDbContext>()));
+            // Konfiguruj logikę weryfikacji nazw użytkowników
+            manager.UserValidator = new UserValidator<Uzytkownik>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+
+            // Konfiguruj logikę weryfikacji haseł
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = true,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = true,
+            };
+
+            // Konfiguruj ustawienia domyślne blokady użytkownika
+            manager.UserLockoutEnabledByDefault = true;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+
+            // Zarejestruj dostawców uwierzytelniania dwuetapowego. W przypadku tej aplikacji kod weryfikujący użytkownika jest uzyskiwany przez telefon i pocztą e-mail
+            // Możesz zapisać własnego dostawcę i dołączyć go tutaj.
+            manager.RegisterTwoFactorProvider("Kod — telefon", new PhoneNumberTokenProvider<Uzytkownik>
+            {
+                MessageFormat = "Twój kod zabezpieczający: {0}"
+            });
+            manager.RegisterTwoFactorProvider("Kod — e-mail", new EmailTokenProvider<Uzytkownik>
+            {
+                Subject = "Kod zabezpieczeń",
+                BodyFormat = "Twój kod zabezpieczający: {0}"
+            });
+            manager.EmailService = new EmailService();
+            manager.SmsService = new SmsService();
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider = 
+                    new DataProtectorTokenProvider<Uzytkownik>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+            return manager;
+        }
+    }
+
+    // Skonfiguruj menedżera logowania aplikacji używanego w tej aplikacji.
+    public class ApplicationSignInManager : SignInManager<Uzytkownik, string>
+    {
+        public ApplicationSignInManager(UzytkownikManager userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(Uzytkownik user)
+        {
+            return user.GenerateUserIdentityAsync((UzytkownikManager)UserManager);
+        }
+
+        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
+        {
+            return new ApplicationSignInManager(context.GetUserManager<UzytkownikManager>(), context.Authentication);
+        }
+    }
+}
---
+++ OGL/Models/Uzytkownik.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace OGL.Models
+{
+    public class Uzytkownik : IdentityUser
+    {
+        public Uzytkownik()
+        {
+            this.Ogloszenia = new HashSet<Ogloszenie>();
+        }
+        // Klucz podstawowy odziedziczymy po klasie IdentityUser
+        // Dodajemy pola Imie i Nazwisko
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+
+        #region dodatkowe pole notmapped
+        [NotMapped] //using System.ComponentModel.DataAnnotations.Schema;
+        [Display(Name = "Pan/Pani:")]
+        public string PelneNazwisko
+        {
+            get { return Imie + " " + Nazwisko; }
+        }
+        #endregion
+
+        public virtual ICollection<Ogloszenie> Ogloszenia { get; private set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<Uzytkownik> manager)
+        {
+            // Element authenticationType musi pasować do elementu zdefiniowanego w elemencie CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Dodaj tutaj niestandardowe oświadczenia użytkownika
+            return userIdentity;
+        }
+    }
+}
