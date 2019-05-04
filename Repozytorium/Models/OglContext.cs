f09d8812709614650948f3f8130e6c30c860b202
maxiormxr <mateuszmachcinski@gmail.com>
2019-05-05 00:09:52 +0200

    przeniesienie warstwy modelu do osobnego projektu

--- Repozytorium/Models/OglContext.cs
+++ Repozytorium/Models/OglContext.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace OGL.Models
+namespace Repozytorium.Models
 {
     // Możesz dodać dane profilu dla użytkownika, dodając więcej właściwości do klasy Uzytkownik. Odwiedź stronę https://go.microsoft.com/fwlink/?LinkID=317594, aby dowiedzieć się więcej.
 
